16f177420af53f77e7fd8f7ddffa64ca5e0593a8
JC86JC <jc86hw@gmail.com>
2021-03-01 02:52:41 +0000

    test build

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import click
 
 @click.command()
-@click.option('--name', default = 'IDS 721', help = 'This is your name')
+@click.option('--name', default = 'Grader', help = 'This is your name')
 @click.option('--repeat', default = 1, help = 'This is the number of times you will be greeted')
 def hello(repeat, name):
     '''This script greets you.'''
