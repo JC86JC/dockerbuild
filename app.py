88d2d21bd62075b63a91236d5284fee93b56169f
JC86JC <jc86hw@gmail.com>
2021-03-01 02:18:23 +0000

    main content

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import click
+
+@click.command()
+@click.option('--name', default = 'IDS 721', help = 'This is your name')
+@click.option('--repeat', default = 1, help = 'This is the number of times you will be greeted')
+def hello(repeat, name):
+    '''This script greets you.'''
+    for i in range(repeat):
+        click.echo('Hello %s!' % name)
+    
+
+if __name__ == '__main__':
+    hello()
