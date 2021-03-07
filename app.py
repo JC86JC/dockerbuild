b4aae59f0e7e85d765a896365322bd63c2022e2c
JC86JC <jc86hw@gmail.com>
2021-03-07 20:50:37 +0000

    Final setup

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 import click
+import numpy as np
 
 @click.command()
-@click.option('--name', default = 'Grader', help = 'This is your name')
-@click.option('--repeat', default = 1, help = 'This is the number of times you will be greeted')
-def hello(repeat, name):
-    '''This script greets you.'''
-    for i in range(repeat):
-        click.echo('Hello %s!' % name)
+@click.option('--start', default = 2, help = 'This is the beginning of the range.')
+@click.option('--end', default = 10, help = 'This is the end of the range.')
+
+def primes_between(start, end):
+    """Return primes between start and end."""
     
+    def is_prime(n):
+        """Check if n is prime."""
+    
+        if n == 2:
+            return True
+        elif n % 2 == 0 or n < 2:
+            return False
+        else:
+            for i in range(3, 1 + int(np.sqrt(n)), 2):
+                if n % i == 0:
+                    return False
+        return True
+    
+    click.echo('Prime numbers are: {}'.format(np.array([i for i in range(start, end) if is_prime(i)])))
 
 if __name__ == '__main__':
-    hello()
+    primes_between()
